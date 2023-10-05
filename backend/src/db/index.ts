95459a4a82fe2c0b12e16bae556d2001f88b3384
Taz17 <mutaz.ashhab@gmail.com>
2023-10-06 10:08:07 +1100

    add jira tickets

---
+++ backend/src/db/index.ts
@@ -0,0 +1,58 @@
+export interface JiraTicket {
+  id: string
+  title: string
+  description: string
+}
+
+export const tickets: JiraTicket[] = [
+  {
+    id: 'ticket-0',
+    title: 'Ticket 0',
+    description: 'This is ticket 0',
+  },
+  {
+    id: 'ticket-1',
+    title: 'Ticket 1',
+    description: 'This is ticket 1',
+  },
+  {
+    id: 'ticket-2',
+    title: 'Ticket 2',
+    description: 'This is ticket 2',
+  },
+  {
+    id: 'ticket-3',
+    title: 'Ticket 3',
+    description: 'This is ticket 3',
+  },
+  {
+    id: 'ticket-4',
+    title: 'Ticket 4',
+    description: 'This is ticket 4',
+  },
+  {
+    id: 'ticket-5',
+    title: 'Ticket 5',
+    description: 'This is ticket 5',
+  },
+  {
+    id: 'ticket-6',
+    title: 'Ticket 6',
+    description: 'This is ticket 6',
+  },
+  {
+    id: 'ticket-7',
+    title: 'Ticket 7',
+    description: 'This is ticket 7',
+  },
+  {
+    id: 'ticket-8',
+    title: 'Ticket 8',
+    description: 'This is ticket 8',
+  },
+  {
+    id: 'ticket-9',
+    title: 'Ticket 9',
+    description: 'This is ticket 9',
+  },
+]
