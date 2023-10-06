451a713948a2e9453b33fe107045863f6ae7908c
Taz17 <mutaz.ashhab@gmail.com>
2023-10-06 11:09:56 +1100

    allow cors

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import { PORT } from './config'
 import { ticketRouter } from './routes/tickets'
+import cors from 'cors'
+
 const app = express()
 const jsonParser = express.json()
 
 app.use(jsonParser)
+
+app.use(cors())
 
 app.use(ticketRouter)
 
