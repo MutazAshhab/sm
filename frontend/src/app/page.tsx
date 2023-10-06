ffb0e29bb2f5852064cfd02582e4cccbcb659ffc
Taz17 <mutaz.ashhab@gmail.com>
2023-10-06 11:30:55 +1100

    style page component and add refresh tickets button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -22,15 +22,26 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col gap-4">
-      {tickets.map((t) => (
-        <Ticket
-          key={t.id}
-          id={t.id}
-          title={t.title}
-          description={t.description}
-        />
-      ))}
-    </div>
+    <>
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+        onClick={async () => {
+          const fetchedTickets = await getAllTickets()
+          setTickets(fetchedTickets)
+        }}
+      >
+        Refetch tickets
+      </button>
+      <div className="flex flex-col gap-4">
+        {tickets.map((t) => (
+          <Ticket
+            key={t.id}
+            id={t.id}
+            title={t.title}
+            description={t.description}
+          />
+        ))}
+      </div>
+    </>
   )
 }
