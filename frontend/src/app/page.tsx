d844aeb45d53bbc89af271c7ac9075e17f0105d8
Taz17 <mutaz.ashhab@gmail.com>
2023-10-06 11:10:28 +1100

    update home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,1 +1,34 @@
-export default function Home() {}
+import { getAllTickets } from '@/api/tickets'
+import { Ticket as TicketType } from '@/types/tickets'
+import { useEffect, useState } from 'react'
+import { Ticket } from './components/Ticket'
+
+export default function Home() {
+  const [tickets, setTickets] = useState<TicketType[] | null>(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedTickets = await getAllTickets()
+      setTickets(fetchedTickets)
+    }
+
+    fetchData()
+  }, [])
+
+  if (tickets === null) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      {tickets.map((t) => (
+        <Ticket
+          key={t.id}
+          id={t.id}
+          title={t.title}
+          description={t.description}
+        />
+      ))}
+    </>
+  )
+}
