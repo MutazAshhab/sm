c9977ee2f0068b0b2e063dba09885fda0fe6ce98
Taz17 <mutaz.ashhab@gmail.com>
2023-10-06 11:36:57 +1100

    add ability to create ticket

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,11 @@ import { getAllTickets } from '@/api/tickets'
 import { Ticket as TicketType } from '@/types/tickets'
 import { useEffect, useState } from 'react'
 import { Ticket } from './components/Ticket'
+import { CreateTicket } from './components/CreateTicket'
 
 export default function Home() {
   const [tickets, setTickets] = useState<TicketType[] | null>(null)
+  const [showCreateTicket, setShowCreateTicket] = useState(false)
 
   useEffect(() => {
     async function fetchData() {
@@ -23,15 +25,26 @@ export default function Home() {
 
   return (
     <>
-      <button
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
-        onClick={async () => {
-          const fetchedTickets = await getAllTickets()
-          setTickets(fetchedTickets)
-        }}
-      >
-        Refetch tickets
-      </button>
+      <div className="flex flex-row gap-4">
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+          onClick={async () => {
+            const fetchedTickets = await getAllTickets()
+            setTickets(fetchedTickets)
+          }}
+        >
+          Refetch Tickets
+        </button>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+          onClick={() => setShowCreateTicket(!showCreateTicket)}
+        >
+          {showCreateTicket ? 'Close Create Ticket' : 'Create Ticket'}
+        </button>
+      </div>
+      {showCreateTicket && (
+        <CreateTicket submitCreate={() => setShowCreateTicket(false)} />
+      )}
       <div className="flex flex-col gap-4">
         {tickets.map((t) => (
           <Ticket
